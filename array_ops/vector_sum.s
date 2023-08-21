cb61e9b3f717d9429587db79da2ad7eb56e39aa7
MrRique15 <henrique-favaro@hotmail.com>
2023-08-21 16:20:18 -0300

    chore: format and translate code

--- array_ops/vector_sum.s
+++ array_ops/vector_sum.s
@@ -1,16 +1,22 @@
 .section .data
-    texto1: .asciz "Soma: %d\n"
+    text1: .asciz "Sum: %d\n"
     v1: .int	10, 20, 30
 
 .text
-.globl _start
+.global _start
 
 _start:
-
+    # PT-BR
     #O trecho a seguir soma os 3 valores do rótulo v1, que funciona como um vetor. 
-    #O endereço inicial do vetor é colocado em %edi e as diferentes posições são acessadas     
-    #incrementando o %edi a cada 4 bytes de forma a causar um deslocamento no acesso deste     
+    #O endereço inicial do vetor é colocado em %edi e as diferentes posições são acessadas   
+    #incrementando o %edi a cada 4 bytes de forma a causar um deslocamento no acesso deste   
     #vetor.
+    
+    # EN-US
+    #The following code sums the 3 values of the label v1, which works as a vector.
+    #The initial address of the vector is placed in %edi and the different positions are accessed 
+    #by incrementing %edi every 4 bytes so as to cause a shift in the access of this  vector
+    
 
     movl $v1, %edi
     movl (%edi), %eax
@@ -20,7 +26,7 @@ _start:
     addl (%edi), %eax
 
     pushl %eax
-    pushl $texto1
+    pushl $text1
     call printf
 
     pushl $0

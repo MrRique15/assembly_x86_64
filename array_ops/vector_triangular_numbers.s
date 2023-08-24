e8d07d048158e20a405b683cfe66802d910d5de6
MrRique15 <henrique-favaro@hotmail.com>
2023-08-24 10:42:50 -0300

    chore: code comment

--- array_ops/vector_triangular_numbers.s
+++ array_ops/vector_triangular_numbers.s
@@ -1,5 +1,5 @@
 .section .data
-    v1: .space 32
+    v1: .space 32         # creates one variable with 32 bits in memory, that you can fill with INT or String, adding $4 in mem address
     zero_number: .int 0
     numbers_to_insert: .int 7
     triangular_amount: .int 0
@@ -13,7 +13,6 @@
 .global _start
 _start:
     movl $v1, %edi
-
     jmp loop_insert
 
 loop_insert:
@@ -39,25 +38,25 @@ loop_insert:
 
         jmp finish_program
 
-
+# function to verify if a number is tringular ex: (1x2x3=6) 6 is triangular, (2x3x4=24) 24 is triangular
 verify_triangular:
     movl (%edi), %ebx
 
     multiplication_loop:
-        movl initial_mult_number, %eax
+        movl initial_mult_number, %eax  # %eax = initial_mult_number
 
         movl %eax, %ecx
         addl $1, %ecx
-        imull %ecx, %eax
+        imull %ecx, %eax  # %eax = initial_mult_number x initial_mult_number+1
         addl $1, %ecx
-        imull %ecx, %eax
+        imull %ecx, %eax  # %eax = initial_mult_number x initial_mult_number+1 x initial_mult_number+2
 
         cmpl %ebx, %eax
-        jg return_to_insertion
-        je found_triangular
+        jg return_to_insertion   # if the multiplication result is bigger than number, returns to insertion loop
+        je found_triangular      # if the multiplication is equal the number, it found a triangular number
 
         addl $1, initial_mult_number
-        jmp multiplication_loop
+        jmp multiplication_loop  # jump to continue multiplication loop when the multiplication result is lower than verifyed number
     ret
 
 found_triangular:
